0703bd5ab1ba7c34d357f456c8a0a865541a9bd1
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2023-11-25 18:00:02 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'image-slide';
+
+  images = [
+    {
+      imgSrc: 'assets/image-1.jpg',
+      imgAlt: 'Image 1',
+    },
+    {
+      imgSrc: 'assets/image-2.jpg',
+      imgAlt: 'Image 2',
+    },
+    {
+      imgSrc: 'assets/image-3.jpg',
+      imgAlt: 'Image 3',
+    },
+    {
+      imgSrc: 'assets/image-4.jpg',
+      imgAlt: 'Image 4',
+    },
+    {
+      imgSrc: 'assets/image-5.jpg',
+      imgAlt: 'Image 5',
+    },
+    {
+      imgSrc: 'assets/image-6.jpg',
+      imgAlt: 'Image 6',
+    },
+    {
+      imgSrc: 'assets/image-7.jpg',
+      imgAlt: 'Image 7',
+    },
+    {
+      imgSrc: 'assets/image-8.jpg',
+      imgAlt: 'Image 8',
+    },
+    {
+      imgSrc: 'assets/image-9.jpg',
+      imgAlt: 'Image 9',
+    },
+    {
+      imgSrc: 'assets/image-10.jpg',
+      imgAlt: 'Image 10',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TouchSliderComponent } from './touch-slider/touch-slider.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TouchSliderComponent, HammerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
